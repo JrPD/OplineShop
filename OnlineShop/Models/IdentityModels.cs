34b7a13145fcf10a26fe5ae958d4def653107504
Bohdan Benetekyi <benetskyybogdan@gmail.com>
2015-10-13 11:55:32 +0200

    зміна патча до БД

--- OnlineShop/Models/IdentityModels.cs
+++ OnlineShop/Models/IdentityModels.cs
@@ -22,8 +22,8 @@ namespace OnlineShop.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
 
-		public DbSet<Category> Categories { get; set; }
-		public DbSet<Product> Products { get; set; }
+		//public DbSet<Category> Categories { get; set; }
+		//public DbSet<Product> Products { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
