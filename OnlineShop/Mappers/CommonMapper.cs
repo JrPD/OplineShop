3ce487409ba9bd197779792e34ecbfdf9113d6bd
Bohdan Benetekyi <benetskyybogdan@gmail.com>
2015-10-17 18:36:33 +0200

    доданий automapper  класс CommonMapper

---
+++ OnlineShop/Mappers/CommonMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Mappers
+{
+    public class CommonMapper : IMapper
+    {
+        public CommonMapper()
+        {
+            //Mapper.CreateMap<User, UserView>();
+        }
+        public object Map(object source, Type sourceType, Type destinationType)
+        {
+            return Mapper.Map(source, sourceType, destinationType);
+        }
+    }
+}
